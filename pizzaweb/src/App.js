3c555046a27554fe330b422af415fa3175ec71c9
Jayawardana OV <114973581+OnaliyVinukiy@users.noreply.github.com>
2023-10-03 20:39:58 +0530

    Created a new project as Pizzaweb

---
+++ pizzaweb/src/App.js
@@ -0,0 +1,20 @@
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Navbar/> 
+      <Routes>
+        <Route path='/' exact component={Home}/>
+      </Routes>
+      </Router>
+        
+    </div>
+  );
+}
+
+export default App;
