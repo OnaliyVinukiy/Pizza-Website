f73f9f0d88bfee2a8ae88d54223a2770baf303aa
Jayawardana OV <114973581+OnaliyVinukiy@users.noreply.github.com>
2023-10-05 21:24:52 +0530

    Updated the Navigation Bar
    Developed the responsive navigation bar

--- pizzaweb/src/components/Navbar.js
+++ pizzaweb/src/components/Navbar.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/pizzaLogo.png'
 import{ Link } from 'react-router-dom'
+import ReorderIcon from '@mui/icons-material/Reorder'
 import '../styles/Navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='leftSide'>
         <img src={Logo} />
+        <div className='hiddenLinks'>
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
         </div>
         <div className='rightSide'>
           <Link to="/">Home</Link>
           <Link to="/menu">Menu</Link>
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
+          <button>
+            <ReorderIcon />
+            </button>
 
       </div>
         
